392beb352c95d0dbc8ded1250c5438c7db2bcfe5
s.riache <s.riache@cyberdian.fr>
2024-12-09 23:23:27 +0100

    feat(analytics): add superuser decorator

--- src/analytics/analytics.controller.ts
+++ src/analytics/analytics.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AnalyticsService } from './analytics.service';
 import { AnalyticsResponseDto } from './dto/analytics.dto';
+import { SuperUserGuard } from 'src/auth/guards/super-user.guard';
+import { AccessTokenGuard } from 'src/auth/guards/access-token.guard';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller('analytics')
 export class AnalyticsController {
   constructor(private readonly analyticsService: AnalyticsService) {}
 
   @Get()
+  @UseGuards(AccessTokenGuard, SuperUserGuard)
+  @ApiBearerAuth()
   async getAnalytics(): Promise<AnalyticsResponseDto> {
     return this.analyticsService.getApplicationAnalytics();
   }
