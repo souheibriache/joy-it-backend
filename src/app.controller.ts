e2e2bf110c503e5788dc29db8a94b3d3e521ee10
s.riache <s.riache@cyberdian.fr>
2024-12-28 17:00:58 +0100

    test(stripe): console log stripe webhook variables

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,9 @@
 import { Controller, Get } from '@nestjs/common'
 import { AppService } from './app.service'
-import { MailerService } from '@app/mailer'
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly mailerService: MailerService,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
